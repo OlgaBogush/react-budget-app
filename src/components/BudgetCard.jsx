f530ee690171c557429faa537610d6da347ae4b0
OlgaBogush <bohush.olga@gmail.com>
2026-02-28 19:31:17 +0300

    feat: add BudgetCard

--- src/components/BudgetCard.jsx
+++ src/components/BudgetCard.jsx
@@ -1,7 +1,51 @@
 import React from "react"
+import { Button, Card, ProgressBar, Stack } from "react-bootstrap"
 
-const BudgetCard = () => {
-  return <div>BudgetCard</div>
+import { formatter } from "../utils"
+
+const getProgressBarVariant = (amount, max) => {
+  const ratio = amount / max
+  if (ratio < 0.5) return "primary"
+  if (ratio < 0.75) return "warning"
+  return "danger"
+}
+
+const BudgetCard = ({ name, amount, max, gray }) => {
+  const classNames = []
+  if (amount > max) {
+    classNames.push("bg-danger", "bg-opacity-10")
+  } else if (gray) {
+    classNames.push("bg-light")
+  }
+
+  return (
+    <Card className={classNames.join(" ")}>
+      <Card.Body>
+        <Card.Title className="title">
+          <div className="me-2">{name}</div>
+          <div className="d-flex align-items-baseline">
+            {formatter.format(amount)}
+            <span className="text-muted fs-6 ms-1">
+              / {formatter.format(max)}
+            </span>
+          </div>
+        </Card.Title>
+        <ProgressBar
+          className="rounded-pill"
+          variant={getProgressBarVariant(amount, max)}
+          min={0}
+          max={max}
+          now={amount}
+        />
+        <Stack direction="horizontal" gap="2" className="mt-4">
+          <Button variant="outline-primary" className="ms-auto">
+            Add Expense
+          </Button>
+          <Button variant="outline-secondary">View Expenses</Button>
+        </Stack>
+      </Card.Body>
+    </Card>
+  )
 }
 
 export default BudgetCard
---
+++ src/utils.js
@@ -0,0 +1,5 @@
+export const formatter = new Intl.NumberFormat("en-IN", {
+  currency: "USD",
+  style: "currency",
+  minimumFractionDigits: 0,
+})
