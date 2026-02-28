35616f5a7b297fed780fb15d8254da15cf8a868e
OlgaBogush <bohush.olga@gmail.com>
2026-02-28 17:53:34 +0300

    init: start budget app

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { Button, Container, Stack } from "react-bootstrap"
+
+import BudgetCard from "./components/BudgetCard"
+
+const App = () => {
+  return (
+    <Container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+      <div className="wrapper">
+        <BudgetCard />
+      </div>
+    </Container>
+  )
+}
+
+export default App
